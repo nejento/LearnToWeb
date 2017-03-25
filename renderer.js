8038d3067a95ad1a5c8666e195b990270d3e6fec
nejento <nejento@gmail.com>
2017-03-25 21:52:28 +0100

    Přidány základní ovládací prvky pro zavírání, minimalizaci a zotavení

--- renderer.js
+++ renderer.js
@@ -1,3 +1,23 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const remote = require('electron').remote;
+
+document.querySelector('.min-button').addEventListener('click', e => {
+    let window = remote.getCurrentWindow();
+    window.minimize();
+});
+
+document.querySelector('.max-button').addEventListener('click', e => {
+    let window = remote.getCurrentWindow();
+    if (!window.isMaximized()) {
+        window.maximize();
+    } else {
+        window.unmaximize();
+    }
+});
+
+document.querySelector('.close-button').addEventListener('click', e => {
+    let window = remote.getCurrentWindow();
+    window.close();
+});
