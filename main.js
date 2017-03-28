2e0c809c13d1a84e72602ab9e2a14f715b66f51a
nejento <nejento@gmail.com>
2017-03-28 22:16:04 +0200

    Opravené scrollování

--- main.js
+++ main.js
@@ -35,7 +35,11 @@ function createWindow () {
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         mainWindow = null
-    })
+    });
+
+    mainWindow.on('resize', e => {
+        //console.log(mainWindow.getSize());
+    });
 }
 
 // This method will be called when Electron has finished
--- renderer.js
+++ renderer.js
@@ -2,6 +2,16 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 const remote = require('electron').remote;
+let currentWindow = remote.getCurrentWindow();
+//let style = getComputedStyle(currentWindow);
+
+currentWindow.on('resize', e => {
+    /*console.log(currentWindow.getSize()[1] - 64);
+    console.log(style);*/
+    //let color = currentWindow.getComputedStyle(document.body).getPropertyValue('--area-size');
+    //document.body.style.setProperty('--area-size', (currentWindow.getSize()[1] - 64) + "px");
+    document.body.style.setProperty('--area-size', (currentWindow.getSize()[1] -64) + "px", '');
+});
 
 document.querySelector('.min-button').addEventListener('click', e => {
     let window = remote.getCurrentWindow();
